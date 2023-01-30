3e4d0a7d41e03c1edc29c39977654f8e4959f079
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-01-30 18:55:20 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className={`grid bg-black w-full h-screen place-items-center`}>
+      <h1 className="font-montserrat text-3xl text-red-500">Turbopack</h1>
+    </div>
+  );
+}
