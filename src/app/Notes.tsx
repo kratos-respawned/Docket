37cae66502f33569e0f0bd2937384885267416b6
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-02-12 14:46:25 +0530

    fixed env and local state

--- src/app/Notes.tsx
+++ src/app/Notes.tsx
@@ -1,24 +1,17 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
 import { Note as TypeNote } from "@/typings/note";
-import { MdDelete, MdEdit, MdOutlineSave } from "react-icons/md";
-import {
-  deleteDoc,
-  doc,
-  updateDoc,
-} from "firebase/firestore";
-import useSWR, { KeyedMutator } from 'swr';
-import { db } from "@/firebase/configs";
+import useSWR from 'swr';
 import fetcher from "@/utils/fetcher";
+import Note from "./Note";
 export default function Notes() {
-  const { data, mutate, isLoading } = useSWR("getData", fetcher);
-  if (!data) return <p>error</p>
-  const list = data;
+  const { data, mutate, isLoading, isValidating, error } = useSWR("getData", fetcher);
+  if (error) return <p className="text-nBlack dark:text-white text-4xl">error</p>
+  const list = data || [];
 
   return (
     <section className="flex flex-wrap gap-4 ">
       {
-        isLoading ? <p>Loading...</p> :
+        (isLoading || isValidating) ? <p className="text-nBlack dark:text-white text-4xl">Loading...</p> :
           (list.length === 0 ? <p className="text-nBlack dark:text-white text-4xl">
             No notes found. <p className="text-xl mt-4"> Click on the + button to add a new note.</p>
           </p> :
@@ -35,103 +28,5 @@ export default function Notes() {
 
 
 
-function Note(props: {
-  editing: boolean;
-  content: string;
-  lastModified: string;
-  accent: string;
-  id: string;
-  del: KeyedMutator<TypeNote[] | undefined>
-}) {
-  const [data, setData] = useState({
-    editing: props.editing,
-    content: props.content,
-    lastModified: props.lastModified,
-    accent: props.accent,
-    id: props.id,
-  });
 
-  const text = useRef<HTMLTextAreaElement>(null);
-  useEffect(() => {
-    if (data.editing) text.current?.focus();
-  }, [data.editing]);
-  const Delete = (id: string) => {
-    const currentNote = data;
-    setData({ ...data, editing: false });
-    const noteDoc = doc(db, "notes", id);
-    deleteDoc(noteDoc).then(() => {
-      props.del((data: any) => {
-        return data.filter((note: any) => note.id !== id);
-      });
-    });
-
-  };
-  const Update = (id: string) => {
-    const currentNote = data;
-    if (!text.current) return;
-    setData({ ...data, editing: false });
-    const noteDoc = doc(db, "notes", id);
-    updateDoc(noteDoc, {
-      editing: false,
-      content: text.current?.value,
-      lastModified: new Date().toDateString(),
-    }).catch(() => {
-      setData({ ...currentNote, editing: false });
-    });
-  };
-  return (
-    <form
-      key={props.id}
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-      className={`note ${props.accent}`}
-    >
-      <textarea
-        name=""
-        id=""
-        ref={text}
-        readOnly={!data.editing}
-        placeholder="Type something..."
-        value={data.content}
-        onChange={(e) => {
-          setData({ ...data, content: e.target.value });
-        }}
-        className="overflow-clip resize-none   bg-transparent outline-none border-none h-full w-full text-base sm:text-xl"
-      />
-      <div className="absolute w-full  py-3 bg-inherit flex justify-between items-center bottom-0 left-0 px-4">
-        <p className="text-sm">{data.lastModified}</p>
-        {data.editing ? (
-          <div className="flex gap-2">
-            <button
-              onClick={() => {
-                Delete(data.id);
-              }}
-              className="edit warn  "
-            >
-              <MdDelete />
-            </button>
-            <button
-              onClick={() => {
-                Update(data.id);
-              }}
-              className="edit "
-            >
-              <MdOutlineSave />
-            </button>
-          </div>
-        ) : (
-          <button
-            onClick={() => {
-              setData({ ...data, editing: true });
-            }}
-            className="edit "
-          >
-            <MdEdit />
-          </button>
-        )}
-      </div>
-    </form>
-  );
-}
 
