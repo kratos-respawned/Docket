88aa50a198db3408718659d0f2d286c5da72d4c4
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-02-12 14:47:12 +0530

    added an error message for env vars

--- src/firebase/configs.ts
+++ src/firebase/configs.ts
@@ -1,5 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+if (!process.env.NEXT_PUBLIC_API_KEY)
+  throw new Error("NEXT_PUBLIC_API_KEY is not defined");
+if (!process.env.NEXT_PUBLIC_AUTH_DOMAIN)
+  throw new Error("NEXT_PUBLIC_AUTH_DOMAIN is not defined");
+if (!process.env.NEXT_PUBLIC_PROJECT_ID)
+  throw new Error("NEXT_PUBLIC_PROJECT_ID is not defined");
+if (!process.env.NEXT_PUBLIC_STORAGE_BUCKET)
+  throw new Error("NEXT_PUBLIC_STORAGE_BUCKET is not defined");
+if (!process.env.NEXT_PUBLIC_MESSENGING_SENDER_ID)
+  throw new Error("NEXT_PUBLIC_MESSENGING_SENDER_ID is not defined");
+if (!process.env.NEXT_PUBLIC_APP_ID)
+  throw new Error("NEXT_PUBLIC_APP_ID is not defined");
+if (!process.env.NEXT_PUBLIC_MEASUREMENT_ID)
+  throw new Error("NEXT_PUBLIC_MEASUREMENT_ID is not defined");
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
