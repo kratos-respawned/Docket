1496f8e450db14e073c440897883d2e380d58472
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-03-13 21:12:28 +0530

    save button

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Edit2 } from "lucide-react"
+import { Edit2, Save } from "lucide-react"
 import { useEffect, useRef, useState } from "react"
 
 function Notes({ accent, content, editing }: { accent: string, content: string, editing: boolean }) {
@@ -18,15 +18,35 @@ function Notes({ accent, content, editing }: { accent: string, content: string, 
                 value={content}
                 disabled={edit ? false : true}
                 className="w-full h-full bg-transparent overflow-clip  outline-none focus:outline-none focus:border-none border-none " />
-            <div className="absolute w-full bottom-0 right-0 px-5 pb-4 pt-2  ">
+            <div className="absolute w-full  bottom-0 right-0 px-5 pb-4 pt-2  ">
                 <button
                     onClick={(e) => {
                         e.preventDefault();
                         setEdit(!edit)
 
                     }}
-                    className="edit block ml-auto bg-transparent  border-none outline-none focus:outline-none focus:border-none">
+                    className={`edit ml-auto bg-transparent  border-none outline-none focus:outline-none focus:border-none ${edit ? "hidden" : " grid"} `}>
                     <Edit2 />
+                </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setEdit(!edit)
+                        console.log(edit)
+
+                    }}
+                    className={`edit ml-auto bg-transparent  border-none outline-none focus:outline-none focus:border-none ${edit ? "grid " : " hidden"} `}>
+                    <Save />
+                </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setEdit(!edit)
+                        console.log(edit)
+
+                    }}
+                    className={`edit ml-auto bg-transparent  border-none outline-none focus:outline-none focus:border-none ${edit ? "grid " : " hidden"} `}>
+                    <Save />
                 </button>
             </div>
 
