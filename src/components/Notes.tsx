370e722ac5cc93d49e1cf4993b85c20d7ad2180e
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-03-13 21:14:58 +0530

    added buttons

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Edit2, Save } from "lucide-react"
+import { Edit2, Save, Delete } from "lucide-react"
 import { useEffect, useRef, useState } from "react"
 
 function Notes({ accent, content, editing }: { accent: string, content: string, editing: boolean }) {
@@ -18,14 +18,14 @@ function Notes({ accent, content, editing }: { accent: string, content: string, 
                 value={content}
                 disabled={edit ? false : true}
                 className="w-full h-full bg-transparent overflow-clip  outline-none focus:outline-none focus:border-none border-none " />
-            <div className="absolute w-full  bottom-0 right-0 px-5 pb-4 pt-2  ">
+            <div className="absolute w-full flex justify-end gap-x-4   bottom-0 right-0 px-5 pb-4 pt-2  ">
                 <button
                     onClick={(e) => {
                         e.preventDefault();
                         setEdit(!edit)
 
                     }}
-                    className={`edit ml-auto bg-transparent  border-none outline-none focus:outline-none focus:border-none ${edit ? "hidden" : " grid"} `}>
+                    className={`edit  ml-auto bg-transparent  border-none outline-none focus:outline-none focus:border-none ${edit ? "hidden" : " grid"} `}>
                     <Edit2 />
                 </button>
                 <button
@@ -35,7 +35,7 @@ function Notes({ accent, content, editing }: { accent: string, content: string, 
                         console.log(edit)
 
                     }}
-                    className={`edit ml-auto bg-transparent  border-none outline-none focus:outline-none focus:border-none ${edit ? "grid " : " hidden"} `}>
+                    className={`edit w-fit  bg-transparent  border-none outline-none focus:outline-none focus:border-none ${edit ? "grid " : " hidden"} `}>
                     <Save />
                 </button>
                 <button
@@ -45,8 +45,8 @@ function Notes({ accent, content, editing }: { accent: string, content: string, 
                         console.log(edit)
 
                     }}
-                    className={`edit ml-auto bg-transparent  border-none outline-none focus:outline-none focus:border-none ${edit ? "grid " : " hidden"} `}>
-                    <Save />
+                    className={`edit w-fit  bg-transparent  border-none outline-none focus:outline-none focus:border-none ${edit ? "grid " : " hidden"} `}>
+                    <Delete />
                 </button>
             </div>
 
