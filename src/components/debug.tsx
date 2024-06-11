0e8320dc28e9a7a0e04ee331d349e95c1db62b9d
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2024-06-11 12:06:56 +0530

    remove tailwind debug

--- src/components/debug.tsx
+++ src/components/debug.tsx
@@ -1,9 +1,10 @@
-export const Debug = () => (
-  <div className="w-12 aspect-square rounded-full fixed bottom-6 left-6 bg-background grid place-items-center shadow-lg">
-    <span className="sm:hidden">xs</span>
-    <span className="hidden sm:block md:hidden">sm</span>
-    <span className="hidden md:block lg:hidden">md</span>
-    <span className="hidden lg:block xl:hidden">lg</span>
-    <span className="hidden xl:block">lg</span>
-  </div>
-);
+export const Debug = () =>
+  process.env.NODE_ENV === "production" ? null : (
+    <div className="w-12 aspect-square rounded-full fixed bottom-6 left-6 bg-background grid place-items-center shadow-lg">
+      <span className="sm:hidden">xs</span>
+      <span className="hidden sm:block md:hidden">sm</span>
+      <span className="hidden md:block lg:hidden">md</span>
+      <span className="hidden lg:block xl:hidden">lg</span>
+      <span className="hidden xl:block">lg</span>
+    </div>
+  );
